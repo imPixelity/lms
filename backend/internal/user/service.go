ce8ddc8a409ecd548317e52b63db3a381cc82fab
phyxl <dzikrynp14@gmail.com>
2026-07-23 11:21:03 +0700

    feat: add user service

--- backend/internal/user/service.go
+++ backend/internal/user/service.go
@@ -1,6 +1,9 @@
 package user
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type service struct {
 	repo Repository
@@ -11,9 +14,38 @@ func NewService(repo Repository) Service {
 }
 
 type Service interface {
-	Get(ctx context.Context, userId int) (*User, error)
+	Create(ctx context.Context, user *User) error
+	FindByID(ctx context.Context, userID int64) (*User, error)
+	// List(ctx context.Context, cursor, limit int) ([]User, error)
+	Update(ctx context.Context, user *User) error
+	Delete(ctx context.Context, userID int64) error
 }
 
-func (s *service) Get(ctx context.Context, userId int) (*User, error) {
-	return s.repo.FindByID(ctx, userId)
+func (s *service) Create(ctx context.Context, user *User) error {
+	if err := s.repo.Create(ctx, user); err != nil {
+		return fmt.Errorf("TODO %w", err)
+	}
+	return nil
+}
+
+func (s *service) FindByID(ctx context.Context, userID int64) (*User, error) {
+	user, err := s.repo.FindByID(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("TODO %w", err)
+	}
+	return user, nil
+}
+
+func (s *service) Update(ctx context.Context, user *User) error {
+	if err := s.repo.Update(ctx, user); err != nil {
+		return fmt.Errorf("TODO %w", err)
+	}
+	return nil
+}
+
+func (s *service) Delete(ctx context.Context, userID int64) error {
+	if err := s.repo.Delete(ctx, userID); err != nil {
+		return fmt.Errorf("TODO %w", err)
+	}
+	return nil
 }
